b4f8a8422193c6e6b3bb33a8d767dac097953f0e
mikko098 <mishalmann@gmail.com>
2025-07-23 02:04:36 +0800

    added a rectangle as sprite

--- super-snake-rocket-slither/src/main.js
+++ super-snake-rocket-slither/src/main.js
@@ -1,12 +1,10 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { startGame } from './phaser_game.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>hi ashin <3</h1>
-    <div id = "pookie" class="card">
+    <div id = "pookie">
     </div>
   </div>
 `
--- super-snake-rocket-slither/src/phaser_game.js
+++ super-snake-rocket-slither/src/phaser_game.js
@@ -1,3 +1,52 @@
+import Phaser from 'phaser';
+
 export function startGame(element) {
-  
+    var snake;
+    var food;
+    var score = 0;
+    var gameOver = false;
+    
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        parent: element,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+
+    function preload() {
+        // load sprites and images here
+        this.load.image('sky', 'assets/sky.png');
+        this.load.spritesheet('balls', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    }
+
+    function create(){
+        this.add.image(400,300,'sky')
+        this.cursors = this.input.keyboard.createCursorKeys();
+        snake = this.physics.add.sprite(20, 20, 'balls');
+        snake.setCollideWorldBounds(true);
+    }
+
+    function update(){
+        if (gameOver)
+            return
+        else if (this.cursors.left.isDown) {
+            snake.x -= 5;
+        }
+        else if (this.cursors.right.isDown) {
+            snake.x += 5;
+        }
+        else if (this.cursors.up.isDown) {
+            snake.y -= 5;
+        }
+        else if (this.cursors.down.isDown) {
+            snake.y += 5;
+        }
+    }
+    
+    var game = new Phaser.Game(config)
 }
