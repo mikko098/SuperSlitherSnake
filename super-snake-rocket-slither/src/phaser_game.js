2a8600bf122de430a61ba851410a2c2c9f9011cf
mikko098 <mishalmann@gmail.com>
2025-07-23 23:40:33 +0800

    added size expandsion

--- super-snake-rocket-slither/src/phaser_game.js
+++ super-snake-rocket-slither/src/phaser_game.js
@@ -1,11 +1,23 @@
 import Phaser from 'phaser';
 
 export function startGame(element) {
-    var snake;
-    var food;
-    var score = 0;
-    var gameOver = false;
-    
+    let snake = [
+        { x: 40, y: 0 },
+        { x: 30, y: 0 },
+        { x: 20, y: 0 },
+        { x: 10, y: 0 },
+        { x: 0, y: 0 }
+    ];
+    let snakeSprites = [];
+    let cursors;
+    let direction = 'RIGHT';
+    let moveTimer = 0;
+    let moveDelay = 100;
+    let gameOver = false;
+    let food;
+    let foodCollected = false;
+    let scene;
+
     const config = {
         type: Phaser.AUTO,
         width: 800,
@@ -19,34 +31,99 @@ export function startGame(element) {
     };
 
     function preload() {
-        // load sprites and images here
         this.load.image('sky', 'assets/sky.png');
-        this.load.spritesheet('balls', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('star', 'assets/star.png');
     }
 
-    function create(){
-        this.add.image(400,300,'sky')
-        this.cursors = this.input.keyboard.createCursorKeys();
-        snake = this.physics.add.sprite(20, 20, 'balls');
-        snake.setCollideWorldBounds(true);
+    function create() {
+        scene = this;
+        this.add.image(400, 300, 'sky');
+        cursors = this.input.keyboard.createCursorKeys();
+
+        // create and store sprite references
+        snake.forEach(part => {
+            const sprite = this.add.sprite(part.x, part.y, 'star').setScale(0.5);
+            snakeSprites.push(sprite);
+        });
+
+        // this.physics.add.overlap(snakeSprites[0], food, collectFood, null, this)
     }
 
-    function update(){
-        if (gameOver)
-            return
-        else if (this.cursors.left.isDown) {
-            snake.x -= 5;
-        }
-        else if (this.cursors.right.isDown) {
-            snake.x += 5;
-        }
-        else if (this.cursors.up.isDown) {
-            snake.y -= 5;
-        }
-        else if (this.cursors.down.isDown) {
-            snake.y += 5;
+    function collectFood(){
+        foodCollected = true;
+    }
+
+    function calculateNextPosition(dir, snake_x, snake_y){
+        switch (dir) {
+            case 'LEFT':
+                snake_x -= 10;
+                return [snake_x, snake_y];
+            case 'RIGHT':
+                snake_x += 10;
+                return [snake_x, snake_y];
+            case 'UP':
+                snake_y -= 10;
+                return [snake_x, snake_y];
+            case 'DOWN':
+                snake_y += 10;
+                return [snake_x, snake_y];
         }
     }
-    
-    var game = new Phaser.Game(config)
+
+    function update(time) {
+        if (gameOver) return;
+
+        // move the snake at a fixed interval
+        if (time - moveTimer > moveDelay) {
+            moveTimer = time;
+
+            // handle direction change (prevents reversing)
+            if (cursors.left.isDown && direction !== 'RIGHT') {
+                direction = 'LEFT';
+            } else if (cursors.right.isDown && direction !== 'LEFT') {
+                direction = 'RIGHT';
+            } else if (cursors.up.isDown && direction !== 'DOWN') {
+                direction = 'UP';
+            } else if (cursors.down.isDown && direction !== 'UP') {
+                direction = 'DOWN';
+            }
+
+            
+            if (cursors.space.isDown){
+                let newPart = {x : snake[0].x, y : snake[0].y}
+                snake.splice(1, 0, newPart)
+                let newSprite = scene.add.sprite(snake[0].x, snake[0].y, 'star').setScale(0.5);
+                snakeSprites.splice(1, 0, newSprite);
+                [snake[0].x, snake[0].y] = calculateNextPosition(direction, snake[0].x, snake[0].y)
+                console.log("running")
+                for (let i = 0; i < snake.length; i++) {
+                    snakeSprites[i].x = snake[i].x;
+                    snakeSprites[i].y = snake[i].y;
+                }
+            }
+            else{
+                for (let i = snake.length - 1; i > 0; i--) {
+                    snake[i].x = snake[i - 1].x;
+                    snake[i].y = snake[i - 1].y;
+                }
+                [snake[0].x, snake[0].y] = calculateNextPosition(direction, snake[0].x, snake[0].y)
+                for (let i = 0; i < snake.length; i++) {
+                    snakeSprites[i].x = snake[i].x;
+                    snakeSprites[i].y = snake[i].y;
+                }
+            }
+            // move the body
+
+            // game over if out of bounds
+            if (
+                snake[0].x < 0 || snake[0].x >= config.width ||
+                snake[0].y < 0 || snake[0].y >= config.height
+            ) {
+                gameOver = true;
+                console.log('Game Over');
+            }
+        }
+    }
+
+    new Phaser.Game(config);
 }
